be317a960485070e70cde7c3c558fcc5e4459967
Anica <popovicanica99@gmail.com>
2022-05-29 18:35:08 +0200

    Kreiran soket

---
+++ src/Writer.py
@@ -0,0 +1,24 @@
+
+from socket import socket 
+
+def SaljiPodatke(data):
+    #ovdje saljemo podatke
+    print("ds")
+
+
+
+
+
+def main():
+    print("Ja sam main")
+    #client = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
+    #client.connect(("localhost",9999))
+    #SaljiPodatke(data)
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
