9149b24eecbb1571f9cbf9f03598a25dc3c0f6cb
My Pc <sasakiticsanz@gmail.com>
2022-05-29 18:18:28 +0200

    Kreiran Socket

---
+++ src/DumpingBuffer.py
@@ -0,0 +1,27 @@
+from socket import socket
+
+
+def PrimiPodatke(data):
+    print(data)
+    
+
+
+
+
+
+
+
+def main():
+    #server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    #server.bind(('localhost',9999))
+    #server.listen()
+
+    
+    #while True:
+        #data = server.accept()
+        #PrimiPodatke(data)
+    print("Main radi")
+
+
+if __name__ == "__main__":
+    main()
