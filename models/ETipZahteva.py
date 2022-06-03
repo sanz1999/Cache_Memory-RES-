af3bc7b0141664e416a2565ecca864191d0d53d0
vuAna <vuana00@gmail.com>
2022-06-03 19:43:42 +0200

    Implementirana logika reader-a

---
+++ models/ETipZahteva.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class ETipZahteva(Enum):
+     KORISNIK = 1 
+     MESEC = 2
+     GRAD = 3
+
---
+++ models/IzvestajPoGradu.py
@@ -0,0 +1,7 @@
+class PoGradu:
+    def __init__(self, brojilo : int, ime : str, prezime : str, adresa : str, potrosnja : int):
+        self.brojilo = brojilo
+        self.ime = ime
+        self.prezime = prezime
+        self.adresa = adresa
+        self.potrosnja = potrosnja
