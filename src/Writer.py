cad9715685bf30e539d533daa0d9622fce61f5aa
Anica <popovicanica99@gmail.com>
2022-06-03 18:41:09 +0200

    Write logika implementirana

--- src/Writer.py
+++ src/Writer.py
@@ -1,23 +1,39 @@
 
-from socket import socket 
+from ast import While
+from cmath import rect
+from pydoc import cli
+import socket
+from typing import List 
 
-def SaljiPodatke(data):
-    #ovdje saljemo podatke
-    print("ds")
+def SaljiPodatke(ID:int,potrosnja:float):
+    
 
+    try:
+        client = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
+        client.connect(("localhost",42500))
+        client.send(ID, potrosnja)
 
+    except: 
+        print("Nije uspjelo slanje")
+   
 
+def UnosPodataka():
+    try:
+        id = int(input("Unesite ID korisnika: "))
+        potrosnja = float(input("Unesite potrosnju za korisnika: "))
+        print(f"Id : {id}  Potrosnja : {potrosnja} ")
+        SaljiPodatke(id, potrosnja)
+    except:
+        print("Pogresan tip podatka")
+
+    return id, potrosnja
 
 
 def main():
-    print("Ja sam main")
-    #client = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
-    #client.connect(("localhost",9999))
-    #SaljiPodatke(data)
-
-
-
-
+    while True:
+        id, potrosnja = UnosPodataka()
+       
+   
 if __name__ == "__main__":
     main()
 
