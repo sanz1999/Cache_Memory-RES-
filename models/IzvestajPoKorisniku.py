4547c62e507becb8cd7f9fafb1b12d7a8ea77790
Stefan Scekic <stafanscekic023@gmail.com>
2022-06-09 22:49:09 +0200

    Prepravljen Izvestaj, TODO provere tipa

--- models/IzvestajPoKorisniku.py
+++ models/IzvestajPoKorisniku.py
@@ -1,16 +1,11 @@
 from email.errors import MessageError
 
 
-class PoKorisniku:
-    def __init__(self, ime : str, prezime : str, adresa : str, grad : str, lista : list):
-     self.ime = ime
-     self.prezime = prezime
+class IzvestajKorisnik():
+    def __init__(self, brojilo : int, adresa : str, grad : str, lista : list):
+     self.brojilo = brojilo
      self.adresa = adresa
-     self.grad = grad 
- 
-     for item in lista :
-         if type(item) != tuple(str, int):
-             print()          
+     self.grad = grad         
      self.lista = lista
 
 
