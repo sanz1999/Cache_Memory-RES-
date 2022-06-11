fe479dd3ab5fc5d50bcf55304553adb3b31034a5
Anica <popovicanica99@gmail.com>
2022-06-11 19:29:24 +0200

    test1

--- src/Writer.py
+++ src/Writer.py
@@ -1,37 +1,42 @@
 
-from ast import While
-from cmath import rect
-from pydoc import cli
-import socket
-from typing import List 
+import socket, pickle, os, sys
+
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from models.ConnectionParams import HOST,  W_PORT
+
 
 def SaljiPodatke(ID:int,potrosnja:float):
-    
-
     try:
         client = socket.socket(socket.AF_INET,socket.SOCK_STREAM) 
-        client.connect(("localhost",42500))
-        client.send(ID, potrosnja)
+        client.connect((HOST,W_PORT))
+        data = ID,potrosnja
+        print(data)
+        data = pickle.dumps(data)
+        print(type(data))
+        
+        client.send(data)
 
     except: 
-        print("Nije uspjelo slanje")
+        print("Nije uspelo slanje")
    
 
 def UnosPodataka():
     try:
         id = int(input("Unesite ID korisnika: "))
         potrosnja = float(input("Unesite potrosnju za korisnika: "))
-        print(f"Id : {id}  Potrosnja : {potrosnja} ")
-        SaljiPodatke(id, potrosnja)
+        print(f"Id: {id}  Potrosnja: {potrosnja} ")
     except:
         print("Pogresan tip podatka")
-
     return id, potrosnja
 
-
+ 
 def main():
     while True:
         id, potrosnja = UnosPodataka()
+        SaljiPodatke(id, potrosnja)
        
    
 if __name__ == "__main__":
